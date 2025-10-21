e76320451c35c49f3c5ec8aad46f07eccfd2dbaf
7utaksrshapankar <utkarshpankar6@gmail.com>
2025-10-21 19:08:16 +0530

    first

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -9,7 +9,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by RoadsideCoder
+        Made with 💗 by Utkarsh
       </div>
     </div>
   );
